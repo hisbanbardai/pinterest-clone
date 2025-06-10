ed979881b9027e13820140c9d6e58fb4643a5883
Hisban Shiraz <hisbanbardai@gmail.com>
2025-06-10 17:00:46 -0600

    Add type to auth component props

--- apps/frontend/src/components/AuthComponent.tsx
+++ apps/frontend/src/components/AuthComponent.tsx
@@ -1,13 +1,21 @@
 import { Link } from "react-router";
 import Logo from "./Logo";
 
+type TAuthComponentProps = {
+  children: React.ReactNode;
+  title: string;
+  bottomText: string;
+  linkText: string;
+  linkTo: string;
+};
+
 export default function AuthComponent({
   children,
   title,
   bottomText,
   linkText,
   linkTo,
-}) {
+}: TAuthComponentProps) {
   return (
     <main className="flex justify-center items-center h-screen">
       <div className="shadow-xl px-10 py-8 flex flex-col gap-5 rounded-2xl min-w-[450px]">
