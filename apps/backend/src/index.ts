2963879dbc8fdec67f54b37cbc549b7be261d8a5
Hisban Shiraz <hisbanbardai@gmail.com>
2025-06-16 15:29:06 -0600

    Start a basic express server

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,2 +1,11 @@
-const a: number = 123;
-console.log(a);
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+app.get("/", (req, res) => {
+  res.json({ msg: "Hello world" });
+  return;
+});
+
+app.listen(PORT, () => console.log("Server running"));
