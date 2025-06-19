4dfebe6f325c091cd43aeecf4a8e6fd3ee3ec1d3
Hisban Shiraz <hisbanbardai@gmail.com>
2025-06-19 16:18:55 -0600

    Add example code to test image upload functionality to ImageKit

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -6,6 +6,7 @@ import PinDetailsPage from "./pages/PinDetailsPage";
 import SigninPage from "./pages/SigninPage";
 import SignupPage from "./pages/SignupPage";
 import HomePage from "./pages/HomePage";
+import ImageKitUpload from "./components/ImageKitUpload";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/create-pin" element={<CreatePinPage />} />
           <Route path="/profile/:username" element={<ProfilePage />} />
           <Route path="/pin/:id" element={<PinDetailsPage />} />
+          <Route path="/test" element={<ImageKitUpload />} />
         </Route>
         <Route path="/signin" element={<SigninPage />} />
         <Route path="/signup" element={<SignupPage />} />
