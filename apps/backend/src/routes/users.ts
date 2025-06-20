b9e25426d575191eec98bd0125a12a1daa4fb7da
Hisban Shiraz <hisbanbardai@gmail.com>
2025-06-20 16:01:55 -0600

    Add code to check existing user

--- apps/backend/src/routes/users.ts
+++ apps/backend/src/routes/users.ts
@@ -1,32 +1,46 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import prisma from "../lib/prisma.ts";
 import { signupSchema } from "@repo/zod/types";
 
 const router = express.Router();
 
-router.post("/signup", async (req, res) => {
-  try {
-    const payload = req.body;
+router.post("/signup", async (req: Request, res: Response): Promise<any> => {
+  const payload = req.body;
 
-    const result = signupSchema.safeParse(payload);
+  const result = signupSchema.safeParse(payload);
 
-    if (result.success) {
-      await prisma.users.create({
-        data: payload,
-      });
-
-      res.status(200).json({ message: "User created successfully" });
-    }
-
-    if (result.error) {
-      throw new Error(result.error.toString());
-    }
-  } catch (error) {
-    console.log(error);
+  if (!result.success) {
+    res.status(400).json({ error: result.error.errors });
+    return;
   }
 
-  // res.json("User route working");
-  // return;
+  try {
+    //check if user already exists
+    const { email } = result.data;
+    const existingUser = await prisma.users.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (existingUser) {
+      res
+        .status(409)
+        .json({ message: "User with that email address already exists" });
+    }
+
+    //create user in the db
+    await prisma.users.create({
+      data: result.data!,
+    });
+
+    res.status(201).json({ message: "User created successfully" });
+    return;
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).json({ error: "Internal server error" });
+    return;
+  }
 });
 
 export default router;
