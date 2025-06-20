16d224733feddb7ee5f76a120ee7bb171d8c9fb7
Hisban Shiraz <hisbanbardai@gmail.com>
2025-06-19 18:19:55 -0600

    Create signup schema and its type and export them

--- packages/zod/src/types.ts
+++ packages/zod/src/types.ts
@@ -1,1 +1,12 @@
-export const a = "hello";
+import z from "zod";
+
+export const signupSchema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(6, "Password must be 6 or more characters in length"),
+});
+
+export type signupSchemaType = z.infer<typeof signupSchema>;
