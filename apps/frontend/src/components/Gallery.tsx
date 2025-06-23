c47e4db82acd2be66ea9ae066ccd7889ed0b30fd
Hisban Shiraz <hisbanbardai@gmail.com>
2025-06-23 14:59:06 -0600

    Add loading images skeleton

--- apps/frontend/src/components/Gallery.tsx
+++ apps/frontend/src/components/Gallery.tsx
@@ -94,10 +94,40 @@ async function fetchPins() {
 
 export default function Gallery() {
   // Queries
-  const { data, error } = useQuery({ queryKey: ["pins"], queryFn: fetchPins });
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["pins"],
+    queryFn: fetchPins,
+    staleTime: 1000 * 60 * 60,
+    refetchOnWindowFocus: false,
+    retry: false,
+  });
 
   if (error) {
     return "An error occurred: " + error.message;
+  }
+
+  if (isLoading) {
+    return (
+      <div className="columns-1 sm:columns-2 md:columns-4 lg:columns-7 px-4">
+        {Array.from({ length: 20 }).map(() => (
+          <div
+            role="status"
+            className="flex items-center justify-center h-56 max-w-sm bg-gray-300 rounded-lg animate-pulse dark:bg-gray-700 mb-4"
+          >
+            <svg
+              className="w-10 h-10 text-gray-200 dark:text-gray-600"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 18"
+            >
+              <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+            </svg>
+            <span className="sr-only">Loading...</span>
+          </div>
+        ))}
+      </div>
+    );
   }
 
   return (
